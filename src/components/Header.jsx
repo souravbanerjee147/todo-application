9db5cfdbaebc09747ed558738f256681a047c7fc
souravbanerjee147 <souravbanerjeeweb18@gmail.com>
2026-01-17 01:45:53 +0530

    Header.jsx is successfully created in components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h1>To-Do List</h1>
+        <p>Organize your life, one task at a time</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
